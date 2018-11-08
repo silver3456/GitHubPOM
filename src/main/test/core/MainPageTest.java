d3ee42ecb51a9c9b07181a347aa58b10d4a1c4f3
silver3456 <avseliverov@gmail.com>
2018-11-08 18:16:11 +0300

    Add MainPage Test

---
+++ src/main/test/core/MainPageTest.java
@@ -0,0 +1,43 @@
+package core;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class MainPageTest {
+    private WebDriver driver;
+    private MainPage mainPage;
+
+    public MainPageTest() {
+    }
+
+    @Before
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-fullscreen");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://github.com/");
+        mainPage = new MainPage(driver);
+    }
+
+    @Test
+    public void signIn() {
+        LoginPage loginPage = mainPage.clickSignIn();
+        String heading = loginPage.getHeadingText();
+        Assert.assertEquals("Sign in to GitHub", heading);
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+
+    }
+}
+
