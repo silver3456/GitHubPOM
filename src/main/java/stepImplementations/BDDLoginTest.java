61b166702008f4488c1fb52a2e32273d719d2439
silver3456 <avseliverov@gmail.com>
2018-12-11 20:43:12 -0800

    Added BDD test for Login page

--- src/main/java/stepImplementations/BDDLoginTest.java
+++ src/main/java/stepImplementations/BDDLoginTest.java
@@ -1,29 +1,44 @@
 package stepImplementations;
 
+import core.LoginPage;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import java.util.concurrent.TimeUnit;
 
 public class BDDLoginTest {
 
-    WebDriver driver;
+    private WebDriver driver;
+    private LoginPage loginPage;
 
     @Given("^user is on login page$")
     public void user_is_on_login_page() {
         System.out.println("User is on the login page");
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-fullscreen");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://github.com/login/");
     }
 
     @When("^user enters correct username and correct password$")
     public void user_enters_correct_username_and_correct_password() {
-        System.out.println("User enteres username and password");
+        System.out.println("User enters username and password");
 
+        loginPage = new LoginPage(driver);
+        loginPage.loginWithValidCreds("alex.malkinn@gmail.com", "Silver@3456");
     }
 
     @Then("^user gets confirmation$")
     public void user_gets_confirmation() {
         System.out.println("User gets confirmation");
-
+        Assert.assertTrue(loginPage.getLoginVerificationLocator());
     }
 }
